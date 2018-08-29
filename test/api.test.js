975072fb06b20ca1bf27c01b1ad9134e6cad8f05
alincode <alincode@gmail.com>
2018-08-30 00:36:39 +0800

    add other api test

--- test/api.test.js
+++ test/api.test.js
@@ -28,7 +28,33 @@ describe('api', function () {
       });
   });
 
-  it('activities', function () {
+  // 每日摘要數據
+  it.skip('Get Daily Activity Summary', function () {
+    const date = '2018-08-28';
+    const options = {
+      uri: `https://api.fitbit.com/1/user/-/activities/date/${date}.json`,
+      qs: {},
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+      json: true
+    };
+
+    return rp(options)
+      .then(function (res) {
+        // console.dir(res);
+        let { summary } = res;
+        summary.should.to.be.an('object');
+        summary.steps.should.to.be.an('number');
+        console.log(summary.steps);
+      })
+      .catch(function (err) {
+        err.should.to.be.empty;
+      });
+  });
+
+  // 終生數據
+  it.skip('Get Lifetime Stats', function () {
     const options = {
       uri: 'https://api.fitbit.com/1/user/-/activities.json',
       qs: {},
@@ -49,5 +75,55 @@ describe('api', function () {
         err.should.to.be.empty;
       });
   });
+
+  // 活動日誌列表
+  it.skip('Get Activity Log List', function () {
+    const options = {
+      uri: 'https://api.fitbit.com/1/user/-/activities/list.json',
+      qs: {
+        'beforeDate': '2018-08-20',
+        'sort': 'asc',
+        'offset': 0,
+        'limit': 100
+      },
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+      json: true
+    };
+
+    return rp(options)
+      .then(function (res) {
+        // console.dir(res);
+        let { activities } = res;
+        activities.should.to.be.an('Array');
+      })
+      .catch(function (err) {
+        err.should.to.be.empty;
+      });
+  });
+
+  // 活動類型
+  it.skip('Browse Activity Types', function () {
+    const options = {
+      uri: 'https://api.fitbit.com/1/activities.json',
+      qs: {},
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+      json: true
+    };
+
+    return rp(options)
+      .then(function (res) {
+        console.dir(res);
+        let { categories } = res;
+        categories.should.to.be.an('Array');
+      })
+      .catch(function (err) {
+        err.should.to.be.empty;
+      });
+  });
+  
 
 });
